7ca1bca6b2ac32e1810cd30764c45063a836b56b
hasanul banna <bannawandoor@gmail.com>
2022-12-22 18:06:00 +0530

    template updations

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('post_details/<int:id>',views.post_details,name='post_details')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .models import BlogData
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    all_blogs = BlogData.objects.all().order_by('-created_at')
+    context = {
+        'blogs' : all_blogs
+    }
+    return render(request,'home.html',context)
+
+
+def post_details(request,id):
+    post_data = BlogData.objects.get(id=id)
+    
+    return render(request,'blog/post_details.html',context={'data' : post_data})
