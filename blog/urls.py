469a5751d0a22c08cf54cf947d2e157241ba9fff
hasanul banna <bannawandoor@gmail.com>
2022-12-22 18:34:26 +0530

    search added in home page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('post_details/<int:id>',views.post_details,name='post_details')
+    path('post_details/<int:id>',views.post_details,name='post_details'),
+    path('search',views.search,name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import BlogData
+from django.db.models import Q
 # Create your views here.
 
 def home(request):
     all_blogs = BlogData.objects.all().order_by('-created_at')
+    
     context = {
-        'blogs' : all_blogs
+        'blogs' : all_blogs,
     }
     return render(request,'home.html',context)
 
@@ -14,3 +16,15 @@ def post_details(request,id):
     post_data = BlogData.objects.get(id=id)
     
     return render(request,'blog/post_details.html',context={'data' : post_data})
+
+def search(request):
+    if request.method == 'GET':
+        search_key = request.GET['search_key']
+        if search_key:
+            search_result = BlogData.objects.order_by('-created_at').filter(Q(heading__icontains=search_key)|Q(description__icontains=search_key)|Q(category__icontains=search_key))
+    else:
+        pass
+    context = {
+        'results':search_result
+    }
+    return render(request,'home.html',context)
