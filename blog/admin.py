540171856794676511896cfdce9e7f055c4acafa
hasanul banna <bannawandoor@gmail.com>
2022-12-22 17:12:58 +0530

    blog models added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['heading', 'category', 'place', 'description', 'highlights',]
+
+
+admin.site.register(BlogData,BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class BlogData(models.Model):
+    heading = models.CharField(default='completely random notes',max_length=500)
+    category = models.CharField(default='life should have meaning',max_length=50)
+    image = models.ImageField(upload_to='photos')
+    place = models.CharField(default='Earth',max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(default='',max_length=10000)
+    highlights = models.CharField(max_length=1000,default='')
+
+    def __str__(self):
+        return self.heading
+
